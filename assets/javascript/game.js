d8f4d57bf7e550209161927398cd180ed8e1175b
Owner <Owner>
2017-02-21 23:51:19 -0600

    final changes before submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@
       var player, defender;
       var enemySelect;
       var enemies = [];
+      var wins = 0; losses = 0;
       //make character objects
       var characters = [
           {name: 'Dark Helmet', imageSrc: 'assets/images/darkHelmet.jpg', playerHp: 180, playerDamage: 10, playerCounterDamage: 25}, 
@@ -19,6 +20,9 @@
       // Use a function to initialize our game or clear for a new round.
       // This way when the user hits clear, we can guarantee a reset of the app.
           function initializeRound() {
+            player = '';
+            defender = '';
+            $('#playerDiv').empty();
           }
 
 
@@ -72,10 +76,10 @@
 
 
         //this will move the selected enemy to the defender area
-
         //this will move the selected enemy to the defender area
         //below: build listening events for clicks on the 'enemySelect' class
-
+        
+        
         $('.enemySelect').on('click', function(){
         defenderIndex = parseInt($(this).attr('enemyIndex'));
         console.log('defenderIndex ' + defenderIndex);
@@ -92,7 +96,7 @@
                 $('#enemiesDiv').empty();
 
                 for (i = 0; i < characters.length; i++) {
-                  if (i !== defenderIndex && i !== playerIndex) {    //go through the image builder for the 'other' index numbers
+                  if (i !== playerIndex && i !== defenderIndex) {    
 
                     imageEnemy = $('<img>');
                     imageEnemy.addClass('imageClass enemySelect');
@@ -104,37 +108,151 @@
                 }
 
        //below will be the section for events happening after the attack button is pushed
+
+                   var player = characters[playerIndex];
+                   var playerHp = player.playerHp;
+                   var playerDamage = player.playerDamage;
+                   var playerCD = player.playerCounterDamage;
+
+                   var defender = characters[defenderIndex];
+                   var defenderHp = defender.playerHp;
+                   var defenderDamage = defender.playerDamage;
+                   var defenderCD = defender.playerCounterDamage;
+
+
+
                $('.attack').on('click', function() {
-                    player = characters[playerIndex];
-                    defender = characters[defenderIndex];
-                    console.log(player);
-                    console.log(defender);
+                //set the fight area variables to avoid changing the character array
+
+
+                    playerHp -= defenderCD;  //player hit by defender
+                    defenderHp -= playerDamage;   // defender hit by player
+                    playerDamage += characters[playerIndex].playerDamage;  //players damage points incr-
+                                                                                                      //ease every attack by charact-
+                    console.log(playerHp);                                                     //ers damage points
+
+                    if (playerHp <= 0) {
+                      $('#attackHeader').html('YOU LOSE!! GAME OVER');
+                      losses++
+                      console.log('losses ' + losses);
+                    }
+
+                    else if (defenderHp <= 0) {
+                      $('#defenderDiv').empty();
+                      $('#attackHeader').html('YOU WIN!! SELECT ANOTHER PLAYER')
+                      wins++
+                    
+                               //defender select events from above
+                                $('.enemySelect').on('click', function(){
+                                defenderIndex2 = parseInt($(this).attr('enemyIndex'));
+                                console.log('defenderIndex2 ' + defenderIndex2);
+                                imageDefender = $('<img>');
+                                imageDefender.addClass('imageClass');
+                                imageDefender.attr('src', characters[defenderIndex2].imageSrc);
+                                $('#defenderDiv').append(imageDefender);
+                                $('#attackHeader').html('Hit the Attack Button!!!');                               
+                                        $('#enemiesDiv').empty();
+                                        for (i = 0; i < characters.length; i++) {
+                                          if (i !== defenderIndex && i !== defenderIndex2 && i !== playerIndex) {    
+                                            imageEnemy = $('<img>');
+                                            imageEnemy.addClass('imageClass enemySelect');
+                                            imageEnemy.attr('src', characters[i].imageSrc);
+                                            imageEnemy.attr('enemyIndex', i);
+                                            $('#enemiesDiv').append(imageEnemy);
+                                          } 
+                                        }
+                              defender = characters[defenderIndex];
+                              defenderHp = defender.playerHp;
+                              defenderDamage = defender.playerDamage;
+                              defenderCD = defender.playerCounterDamage;
+                                  
+                             $('.attack').on('click', function() {
+                              playerHp -= defenderCD;  //player hit by defender
+                              defenderHp -= playerDamage;   // defender hit by player
+                              playerDamage += characters[playerIndex].playerDamage;  
+
+                              if (playerHp <= 0) {
+                                $('#attackHeader').html('YOU LOSE!! GAME OVER');
+                                losses++
+                                console.log('losses ' + losses);
+                              }
+                              else if (defenderHp <= 0) {
+                                $('#defenderDiv').empty();
+                                $('#attackHeader').html('YOU WIN!! SELECT ANOTHER PLAYER')
+                                wins++
+                  /////////
+                                                 //defender select events from above
+                                $('.enemySelect').on('click', function(){
+                                defenderIndex3 = parseInt($(this).attr('enemyIndex'));
+                                console.log('defenderIndex3 ' + defenderIndex3);
+                                imageDefender = $('<img>');
+                                imageDefender.addClass('imageClass');
+                                imageDefender.attr('src', characters[defenderIndex3].imageSrc);
+                                $('#defenderDiv').append(imageDefender);
+                                $('#attackHeader').html('Hit the Attack Button!!!');                               
+                                        $('#enemiesDiv').empty();
+                                        for (i = 0; i < characters.length; i++) {
+                                if (i !== defenderIndex && i !== defenderIndex2 && i !== defenderIndex3 && i !== playerIndex) {    
+                                            imageEnemy = $('<img>');
+                                            imageEnemy.addClass('imageClass enemySelect');
+                                            imageEnemy.attr('src', characters[i].imageSrc);
+                                            imageEnemy.attr('enemyIndex', i);
+                                            $('#enemiesDiv').append(imageEnemy);
+                                          } 
+                                        }
+                              defender = characters[defenderIndex];
+                              defenderHp = defender.playerHp;
+                              defenderDamage = defender.playerDamage;
+                              defenderCD = defender.playerCounterDamage;
+                                  
+                             $('.attack').on('click', function() {
+                              playerHp -= defenderCD;  //player hit by defender
+                              defenderHp -= playerDamage;   // defender hit by player
+                              playerDamage += characters[playerIndex].playerDamage;  
+
+                              if (playerHp <= 0) {
+                                $('#attackHeader').html('YOU LOSE!! GAME OVER');
+                                losses++
+                                console.log('losses ' + losses);
+                              }
+                              else if (defenderHp <= 0) {
+                                $('#defenderDiv').empty();
+                                $('#attackHeader').html('YOU WIN!! GAME OVER')
+                                wins++
+
+                                var resetButton = $('<button>');
+                                resetButton.addClass('reset btn btn-primary');
+                                resetButton.text('RESET');
+                                $('#attackHeader').html(resetButton);
+
+                                $('.reset').on('click', function () {
+                                  initializeRound();
+                                })
+
+
+
+
+                              }
+                            })
+                           })
 
 
 
 
 
-
-
-
-
+                    }
+                  })
+                  })
+                 }
                })
 
-
-      // Add an on click listener to all elements that have the class "reset"
-    //  $(".reset").on("click", function() {
-
-        // Call initializeCalculater so we can reset the state of our app
-    //    initializeRound();
-//
-  //    });
-
-      // Call initializeCalculater so we can set the state of our app
-     // initializeRound();
         })
+      
 
       });  
 
 
     }) 
 
+
+                              
